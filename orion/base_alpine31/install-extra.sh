a56fb0d253df107b66c8aebe8a8c14afc23c3b5b
Ignacio Martín <ignacio.martin@udg.edu>
2015-04-16 19:48:33 +0200

    Copy to /usr to match package installs

--- orion/base_alpine31/install-extra.sh
+++ orion/base_alpine31/install-extra.sh
@@ -3,4 +3,4 @@
 cd
 wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.6.9.tgz
 tar xvf mongodb-linux-x86_64-2.6.9.tgz
-cp -R mongodb-linux-x86_64-2.6.9/* /usr/local
+cp -R mongodb-linux-x86_64-2.6.9/* /usr/
